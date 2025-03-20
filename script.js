788154da60d766fda3015b2d4345b5f483ee7542
sanjeetkumar61 <sanjeetkr9910@gmail.com>
2025-03-20 14:54:30 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const navElement = document.querySelector('nav');
 const navbarLinks = navElement.querySelectorAll('a');
 const sectionElements = document.querySelectorAll('section');
@@ -18,3 +19,36 @@ const removeActiveLinks = () => {
 const hideSections = () => {
     sectionElements.forEach(section => section.classList.add('hidden'));
 };
+=======
+const addButtonElement = document.getElementById('counterAdd')
+const subButtonElement = document.getElementById('counterSub');
+const counterDisplayElement = document.getElementById('counterDisplay');
+
+let total = 0;
+const limit = 20;
+
+const updateCounterDisplay = function() {
+    if (total > limit) {
+        total = limit;
+    }
+
+    if (total < 0) {
+        total = 0;
+    }
+
+    counterDisplayElement.textContent = total;
+    document.body.style.setProperty('background-color', `rgb(${(total / limit) * 255}, 64, 0)`)
+};
+
+addButtonElement.addEventListener('click', () => {
+    total += 1;
+    updateCounterDisplay();
+});
+
+subButtonElement.addEventListener('click', () => {
+    total -=1;
+    updateCounterDisplay();
+});
+
+updateCounterDisplay();
+>>>>>>> 61b5bc8 (first commit)
